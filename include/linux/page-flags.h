f125f6307f8299bf8e341ff278fbbcab97be274a
Andy Whitcroft <apw@canonical.com>
2011-07-27 17:59:40 +0100

    Revert "UBUNTU: SAUCE: add tracing for user initiated readahead requests"
    This reverts commit 4064b8753fbcbd72cc65baf3cb04daca0b361a2c.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -107,7 +107,6 @@ enum pageflags {
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	PG_compound_lock,
 #endif
-	PG_readaheadunused,	/* user oriented readahead as yet unused*/
 	__NR_PAGEFLAGS,
 
 	/* Filesystems */
@@ -235,8 +234,6 @@ PAGEFLAG(MappedToDisk, mappedtodisk)
 /* PG_readahead is only used for file reads; PG_reclaim is only for writes */
 PAGEFLAG(Reclaim, reclaim) TESTCLEARFLAG(Reclaim, reclaim)
 PAGEFLAG(Readahead, reclaim)		/* Reminder to do async read-ahead */
-
-PAGEFLAG(ReadaheadUnused, readaheadunused)
 
 #ifdef CONFIG_HIGHMEM
 /*
